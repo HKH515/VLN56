730a9947d33dfeadeb11050399f71eab0fbd9e9f
PowerPuff <herdis13@ru.is>
2015-11-27 11:08:36 +0000

    Added get and set functions

--- person.cpp
+++ person.cpp
@@ -1,7 +1,24 @@
 #include "person.h"
 
+using namespace std;
+
 Person::Person()
 {
+    // set functions
+    void set_name(string n) {name = n;};
+    void set_birthyear(int y) {birthyear = y;};
+    void set_deathyear(int y) {dethyear = y;};
+
+    // get functions
+    string get_name() {return name;};
+    int get_birthyear() {return birth_year;};
+    int get_deathyear() {return death_year;};
+    string get_description() {return description;};
+    string get_profession() {return profession};
+
+
+
+
 
 }
 
--- person.h
+++ person.h
@@ -1,6 +1,7 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <string>
 
 class Person
 {

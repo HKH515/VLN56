5e5f68d21d6f3f8427b86ead483e632723864a3f
thoreyg <thoreyjona@gmail.com>
2015-11-27 11:10:36 +0000

    variables and constructors

--- person.h
+++ person.h
@@ -1,11 +1,50 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <string>
+
+using namespace std;
 
 class Person
 {
+private:
+    string name;
+    int birth_year;
+    int death_year;
+    bool sex;
+    string description;
+    string profession; // Mathematician, Physicist, bioligist, computer scientist
+
 public:
-    Person();
+    Person()
+    {
+        name = "";
+        birth_year = 0;
+        death_year = 0;
+        sex = 1;
+        description = "";
+        profession = "";
+    }
+    Person (string n, int by, int dy, bool s, string desc, string prof)
+    {
+        name = n;
+        birth_year = by;
+        death_year = dy;
+        sex = s;
+        description = desc;
+        profession = prof;
+    }
+    // Set functions
+    void set_name(string n);
+    void set_birthyear(int y);
+    void set_deathyear(int y);
+
+    // Get functions
+    string get_name();
+    int get_birthyear();
+    int get_deathyear();
+    string get_description();
+    string get_profession();
 };
 
 #endif // PERSON_H

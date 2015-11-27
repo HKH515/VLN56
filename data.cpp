e26d153515e414765480365a5143de49297cda3d
Hannes Kristján Hannesson <hanneskh15@ru.is>
2015-11-27 13:18:05 +0000

    added some functions to data

--- data.cpp
+++ data.cpp
@@ -1,7 +1,49 @@
 #include "data.h"
 
-Data::Data()
+Data::Data(string datafile)
 {
-
+    setFile(datafile);  
 }
 
+void Data::setFile(string data)
+{
+    filename = data;   
+}
+
+string Data::getFile()
+{
+    return filename;
+}
+
+vector<string> Data::read()
+{
+    ifstream inputStream;
+    inputStream.open(getFile())
+    if (inputStream.is_open())
+    {
+        while (getline(inputStream, line))
+        {
+            push_back(line);
+        }
+    }
+    
+}
+
+void Data::push_back(string entry)
+{
+    this->data.push_back(entry);
+}
+
+vector<string> Data::query(string dataQuery)
+{
+    vector<string> queryVect;
+    for (int i = 0; i <= this->data.length(); i++)
+    {
+        if (regex_match(data[i], dataQuery)
+        {
+            queryVect.push_back(data[i]);     
+
+        }
+    }
+    return queryVect;
+}
--- data.h
+++ data.h
@@ -1,11 +1,26 @@
 #ifndef DATA_H
 #define DATA_H
 
+#include <fstream>
+#include <string>
+#include <vector>
+#include <regex>
+
+using namespace std;
 
 class Data
 {
-public:
-    Data();
+    private:
+        string filename;
+        vector<string> data;
+    
+    public:
+        Data(string datafile);
+        string getFile();
+        void setFile(string data);
+        void push_back(string entry);
+        vector<string> query(); //Fetches all lines matching query
+        vector<string> read(); //Reads the whole data file
 };
 
 #endif // DATA_H

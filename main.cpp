d6daec281fd959c79169c43bfcc05a08430f8719
agnare <agnar.erlingsson@simnet.is>
2015-11-30 12:10:34 +0000

    added the presentation layer to main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "data.h"
 #include "person.h"
 #include "domain.h"
+#include "presentation.h"
 
 using namespace std;
 
@@ -30,14 +31,19 @@ int main(int argc, char *argv[])
 { 
     QCoreApplication a(argc, argv);
 
-    //vector names[];
+    string inputs;
+
+    presentation commands;
 
     printSplash();
     cout << "Welcome to the Archive of all the great IT people!" << endl;
 
-
+    inputs = commands.input();
 
     Domain *d = new Domain();
+
+    commands.choice(inputs, d);
+
     //choice(inputs, d);
 
     return a.exec();

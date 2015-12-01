e84a6df9f35892f61b624ca6de8fc9a36b3d0a9b
PowerPuff <herdis13@ru.is>
2015-12-01 11:27:47 +0000

    Fixed search

--- presentation.cpp
+++ presentation.cpp
@@ -16,45 +16,27 @@ Domain* presentation::get_domain()
    return d;
 }
 
-/*string presentation::input()
-{
-    string inputs;
-    cin >> inputs;
-
-    if(inputs == "help")
-    {
-        cout << endl;
-        cout << "-------------------------------------------------" << endl;
-        cout << "add: Add to the database" << endl;
-        cout << "search: Search the list for prefered information" << endl;
-        cout << "list: Display the whole list in prefered order" << endl;
-        cout << "exit: Close the program" << endl;
-        cout << "help: Open up the help menu" << endl;
-        cout << "-------------------------------------------------" << endl;
-        cin >> inputs;
-    }
-
-    return inputs;
-}*/
-
 void presentation::choice(Domain* d)
 {
     string inputs;
     cin >> inputs;
     vector<string> command_vec;
     string order_of_sort;
-    int sort_by;
+    string sort_by;
+
     // Put all letter to lowercase - ATH gera þegar error checka skipun?
     for (unsigned int i = 0; i < inputs.length(); i++)
     {
         inputs[i] = tolower(inputs[i]);
     }
 
+    // Infinite while loop that receives and handles commands. If the user enters exit the while
+    // loop terminates.
     do
     {
+        command_vec.push_back(inputs);
         if(inputs == "add")
         {
-            command_vec.push_back("add");
             command_vec = parse_add();
             d->handle_commands(command_vec);
         }
@@ -62,13 +44,13 @@ void presentation::choice(Domain* d)
         {
             sort_msg(1);
             cin >> sort_by;
-            while ( (sort_by != 1) && (sort_by != 2) && (sort_by != 3) && (sort_by != 4) && (sort_by != 5)) {
+            while ( (sort_by != "1") && (sort_by != "2") && (sort_by != "3") && (sort_by != "4") && (sort_by != "5")) {
                 cout << "This is not a valid choice, please choose again" << endl;
                 cin >> sort_by;
             }
+            command_vec.push_back(sort_by);
             sort_msg(2);
             cin >> order_of_sort;
-
             // Error checking that the user put in either "a" or "d"
             while (order_of_sort != "a" && order_of_sort != "d")
             {
@@ -76,19 +58,20 @@ void presentation::choice(Domain* d)
                 cin >> order_of_sort;
             }
             command_vec.push_back(order_of_sort);
-            d->handle_commands(command_vec); // Ath hægt að setja þessa línu neðst?
+            cout << "Búin að pusha inn í vectorinn kalla á handle commands" << endl;
+
+            d->handle_commands(command_vec);
             print_results(d);
         }
         else if(inputs == "search")
         {
             string search_column;
-            command_vec.push_back("search");
             sort_msg(3);
             cin >> search_column;
             // -----------------------Muna að gera tolower hér-------------------------
-            while (search_column != "name" && search_column != "profession" && search_column != "description" && search_column!= "birthyear" && search_column!= "deathyear" && search_column!= "sex")
+            while (search_column != "1" && search_column != "2" && search_column != "3" && search_column!= "4" && search_column!= "5" && search_column!= "6")
             {
-                cout << "Invalid command - please choose one of the following: name, profession, description, birthyear, deathyear, sex" << endl;
+                cout << "This is not a valid choice, please choose again" << endl;
                 cin >> search_column;
             }
             command_vec.push_back(search_column);
@@ -99,10 +82,11 @@ void presentation::choice(Domain* d)
             command_vec.push_back(search_query);
             sort_msg(1);
             cin >> sort_by;
-            while ( (sort_by != 1) && (sort_by != 2) && (sort_by != 3) && (sort_by != 4) && (sort_by != 5)) {
+            while ( (sort_by != "1") && (sort_by != "2") && (sort_by != "3") && (sort_by != "4") && (sort_by != "5")) {
                 cout << "This is not a valid choice, please choose again" << endl;
                 cin >> sort_by;
             }
+            command_vec.push_back(sort_by);
             sort_msg(2);
             cin >> order_of_sort;
             // Error checking that the user put in either "a" or "d"
@@ -147,7 +131,7 @@ vector <string> presentation::parse_add()
     string input;
     vector<string> add_vec;
     add_vec.push_back("add");
-    cout << "You will be asked to enter information about the person, if you want to leave out information enter '-' when asked."<< endl;
+    cout << "You will be asked to enter information about the person" << endl;
     cout << "Please write the name of the person: " << endl;
     cin.ignore();
     getline(cin, input);

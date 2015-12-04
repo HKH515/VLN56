f8f63a00e1b23e9d58430af38fcced3cf59f59f0
Hannes Kristján Hannesson <hanneskh15@ru.is>
2015-12-04 16:49:56 +0000

    worked on data.cpp

--- data.cpp
+++ data.cpp
@@ -27,6 +27,11 @@ QString Data::getFile() {
 
 void Data::write(string table, string line) {
     vector<string> fields = parseDelimString(line, '|');
+    cout << "field no. 0 is" << fields[0];
+    for (int i = 0; i < fields.size(); i++)
+    {
+        cout << fields[i] << endl;
+    }
     db.open();
     if( !db.open() )
     {
@@ -36,25 +41,43 @@ void Data::write(string table, string line) {
     cout << "db opened";
     QSqlQuery queryObj(db);
     string queryString;
-    if (table == "persons")
-    {
-        queryString = "INSERT INTO " + table + " (name, profession, description, birthyear, deathyear, sex, id) values('"
-                + fields[0] + "','" + fields[1] + "','" + "','" + fields[2] + "','" + fields[3] + "','" + fields[4] + ','
-                + fields[5] + "','" + fields[6] + "');";
+    cout << "table: " << table << endl;
+    cout << "line: " << line << endl;
+    //if (table == "persons")
+    //{
+        cout << "inside wadfsadf";
+        queryString = "INSERT INTO 'persons' (name, profession, description, birthyear, deathyear, sex) values('" + fields[0] + "', '" + fields[1] + "', '" + fields[2] + "', '" + fields[3] + "', '" + fields[4] + "', '" + fields[5] + "');";
+                //queryString = "INSERT INTO 'persons' (name, profession, description, birthyear, deathyear, sex) values('"
+                //        + fields[0] + "','" + fields[1] + "','" + "','" + fields[2] + "','" + fields[3] + "','" + fields[4] + ','
+                //        + fields[5] + "');";
+        cout << "YO QUERY: " << queryString << endl;
 
-    }
+    //}
     cout << queryString << endl;
 
     QString qQueryString(queryString.c_str());
     queryObj.exec(qQueryString);
+    qDebug() << queryObj.lastError() << endl;
     db.close();
+}
+
+string Data::createDelimString(vector<string> sourceVec, string delim)
+{
+    string results = " ";
+    for (int i = 0; i < sourceVec.size(); i++)
+    {
+        results = results + sourceVec[i] + delim;
+    }
+    return results;
 }
 
 vector<string> Data::parseDelimString(string delimString, char delim)
 {
     vector<string> results;
+
     for (int i = 0; i < count(delimString.begin(), delimString.end(), delim); i++)
     {
+
         results.push_back(delimString.substr(nthIndex(delimString, delim, i)+1, nthIndex(delimString, delim, i+1) - nthIndex(delimString, delim, i)-1));
     }
     return results;
@@ -76,6 +99,7 @@ vector<string> Data::readEntries(string table, string column, string order) {
     qDebug() << queryObj.lastError() << endl;
     queryVect = fromDbToVector(table, queryObj);
     cout << queryVect.size() << endl;
+    db.close();
     return queryVect;
 
 }
@@ -102,28 +126,30 @@ vector<string> Data::fromDbToVector(string table, QSqlQuery queryObj)
     vector<string> result;
     while(queryObj.next())
     {
+        vector<string> tableData;
         string currentEntry = " ";
         if (table == "persons")
         {
-            currentEntry += queryObj.value("name").toString().toStdString() + "|";
-            currentEntry += queryObj.value("profession").toString().toStdString() + "|";
-            currentEntry += queryObj.value("description").toString().toStdString() + "|";
-            currentEntry += queryObj.value("birthyear").toString().toStdString() + "|";
-            currentEntry += queryObj.value("deathyear").toString().toStdString() + "|";
-            currentEntry += queryObj.value("sex").toString().toStdString() + "|";
-            currentEntry += queryObj.value("id").toString().toStdString() + "|";
-         }
+            tableData.push_back(queryObj.value("name").toString().toStdString());
+            tableData.push_back(queryObj.value("profession").toString().toStdString());
+            tableData.push_back(queryObj.value("description").toString().toStdString());
+            tableData.push_back(queryObj.value("birthyear").toString().toStdString());
+            tableData.push_back(queryObj.value("deathyear").toString().toStdString());
+            tableData.push_back(queryObj.value("sex").toString().toStdString());
+            tableData.push_back(queryObj.value("id").toString().toStdString());
+        }
         else if (table == "computers")
         {
-            currentEntry += queryObj.value("name").toString().toStdString() + "|";
-            currentEntry += queryObj.value("profession").toString().toStdString() + "|";
-            currentEntry += queryObj.value("description").toString().toStdString() + "|";
-            currentEntry += queryObj.value("birthyear").toString().toStdString() + "|";
-            currentEntry += queryObj.value("deathyear").toString().toStdString() + "|";
-            currentEntry += queryObj.value("sex").toString().toStdString() + "|";
-            currentEntry += queryObj.value("id").toString().toStdString() + "|";
+            tableData.push_back(queryObj.value("name").toString().toStdString());
+            tableData.push_back(queryObj.value("profession").toString().toStdString());
+            tableData.push_back(queryObj.value("description").toString().toStdString());
+            tableData.push_back(queryObj.value("birthyear").toString().toStdString());
+            tableData.push_back(queryObj.value("deathyear").toString().toStdString());
+            tableData.push_back(queryObj.value("sex").toString().toStdString());
+            tableData.push_back(queryObj.value("id").toString().toStdString());
         }
-        cout << currentEntry << endl;
+
+        currentEntry = createDelimString(tableData, "|");
         result.push_back(currentEntry);
     }
     return result;
@@ -131,15 +157,19 @@ vector<string> Data::fromDbToVector(string table, QSqlQuery queryObj)
 
 vector<string> Data::query(string table, string column, string dataQuery, string sortColumn, string order) {
     vector<string> queryVect;
-    qDebug() << db.open();
+    if (db.open())
+    {
+        cout << "WORKS" << endl;
+
+    }
     QSqlQuery queryObj(db);
-    string queryString = "SELECT * FROM " + table + " WHERE " + column + " LIKE %" + dataQuery + "% ORDER BY " + sortColumn + ";";
+    string queryString = "SELECT * FROM '" + table + "' WHERE '" + sortColumn + "' LIKE '%" + dataQuery + "%' ORDER BY '" + column + "';";
+    cout << queryString << endl;
     QString qQueryString(queryString.c_str());
     queryObj.exec(qQueryString);
-
+    qDebug() << queryObj.lastError() << endl;
     queryVect = fromDbToVector(table, queryObj);
-
+    cout << queryVect.size() << endl;
     db.close();
-
     return queryVect;
 }

b60f8c90ec77889cde9aed265eb30dbaa1d9ee59
PowerPuff <herdis13@ru.is>
2015-12-11 14:34:23 +0000

    implement connectionservice

--- connectionsservice.cpp
+++ connectionsservice.cpp
@@ -2,6 +2,43 @@
 
 ConnectionsService::ConnectionsService()
 {
+    computer_repo = new ComputerRepository("data.sqlite", "computer_connection");
+    person_repo = new PersonRepository("data.sqlite", "person_computer");
+}
+
+void ConnectionsService::free_vector_memory()
+{
+    for (unsigned int i = 0; i < person_vec.size(); i++)
+    {
+        delete person_vec[i];
+    }
+    for (unsigned int i = 0; i < computer_vec.size(); i++)
+    {
+        delete computer_vec[i];
+    }
+}
+
+void ConnectionsService::get_all_connections()
+{
+    free_vector_memory();
+
+
+
+}
+
+void ConnectionsService::add_connection(string person_id, string computer_id)
+{
+    free_vector_memory();
+
+}
+
+void ConnectionsService::remove_connection(string person_id, string computer_id)
+{
+
+}
+
+void ConnectionsService::get_connected(vector<string> v)
+{
 
 }
 

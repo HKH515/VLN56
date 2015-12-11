099c3940a465accaec159c27fadadf94cc1a82ac
thoreyg <thoreyjona@gmail.com>
2015-12-11 14:15:01 +0000

    created computerservice

--- computerservice.cpp
+++ computerservice.cpp
@@ -2,6 +2,92 @@
 
 ComputerService::ComputerService()
 {
-
+    computer_repo = new ComputerRepository("data.sqlite", "computer_repo_connection");
 }
 
+string ComputerService::parse_add_command(vector<string> v)
+{
+    string st = " "; /* add space in front of the string */
+    for (unsigned int i = 2; i < v.size(); i++)
+    {
+        st += v[i];
+        st += "|"; /* add | after each vector element */
+    }
+    return st;
+}
+
+void ComputerService::parse_query_vector(vector<string> v)
+{
+    for (unsigned int i = 0; i < v.size(); i++)
+    {
+        string st = v[i];
+        size_t position_beg = 0;
+        size_t position_end = 0;
+        /* cut out the empty space in the beginning of the string */
+        st = st.substr(1, st.length());
+        Computer* c = new Computer();
+
+        /* find the name */
+        position_end = st.find("|");
+        c->set_name(st.substr(0, position_end - 1));
+
+        /* find the construction year */
+        position_beg = st.find("|", position_end + 1);
+        c->set_construction_year(stoi(st.substr(position_end + 1, (position_beg - position_end - 1))));
+
+        /* find the type */
+        position_end = st.find("|", position_beg + 1);
+        c->set_type(st.substr(position_beg + 1, (position_end - position_beg - 1)));
+
+        /* find if built */
+        position_beg = st.find("|", position_end + 1);
+        c->set_built(stoi(st.substr(position_end + 1, (position_beg - position_end - 1))));
+
+        /* find the description */
+        position_end = st.find("|", position_beg + 1);
+        c->set_description(st.substr(position_beg + 1, (position_end - position_beg - 1)));
+
+        /* find the id */
+        position_beg = st.find("|", position_end + 1);
+        c->set_id(stoi(st.substr(position_end + 1, (position_beg - position_end - 1))));
+
+        computer_vec.push_back(c);
+    }
+}
+
+void ComputerService::free_vector_memory()
+{
+    for (unsigned int i = 0; i < computer_vec.size(); i++)
+    {
+        delete computer_vec[i];
+    }
+    computer_vec.clear();
+}
+
+void ComputerService::get_all_computers()
+{
+    parse_query_vector(computer_repo->read_entries());
+}
+
+void ComputerService::add_computer(vector<string> v)
+{
+    computer_repo->write(parse_add_command(v));
+}
+
+void ComputerService::search_computer(string column, string substring)
+{
+    /*if (column == "built")  Let the user enter specific strings if searching for built
+    {
+        substring = check_type_query(v[3]);
+    }
+    else
+    {
+        substring = v[3];
+    }
+    parse_query_vector(computer_repo->query(column, substring));*/
+}
+
+void ComputerService::remove_computer(string rem_id)
+{
+    computer_repo->remove("id", rem_id);
+}
--- computerservice.h
+++ computerservice.h
@@ -1,9 +1,31 @@
 #ifndef COMPUTERSERVICE_H
 #define COMPUTERSERVICE_H
 
+#include "computerrepository.h"
+#include "computer.h"
 
+#include <vector>
 class ComputerService
 {
+
+private:
+    vector<Computer*> computer_vec;
+    ComputerRepository* computer_repo;
+
+    string parse_add_command(vector<string> vec);
+    /* Receives a vector of strings from data layer and parses the string, fills into
+       the persons class and returns a vector of persons* to the presentation layer */
+    void parse_query_vector(vector<string> v);
+    /* Deletes all persons object from the vectors and clears the vectors before each command to be handled */
+    void free_vector_memory();
+    /* Handles the list command */
+    void get_all_computers();
+    /* Handles the add command */
+    void add_computer(vector<string> v);
+    /* Handles the search command */
+    void search_computer(string column, string substring);
+    /* Handles the remove command */
+    void remove_computer(string rem_id);
 public:
     ComputerService();
 };

b819838d4d22b566807b069bef839ee7c5d5fdb6
thoreyg <thoreyjona@gmail.com>
2015-12-13 17:47:38 +0000

    Implemented remove for Scientist and Computer

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@
 #include <QMessageBox>
 #include <QString>
 #include <QDebug>
+#include <sstream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -187,7 +188,20 @@ void MainWindow::on_remove_pushButton_clicked()
         ui->see_more_view_computer->hide();
         ui->description_output_person->clear();
         currently_chosen_entry = ui->table_view_person->currentIndex().row();
-        Person* current_person = person_vector.at(currently_chosen_entry);
+        QTableWidgetItem *entry_name = ui->table_view_person->item(currently_chosen_entry, 0);
+        string name = entry_name->text().toStdString();
+        vector<Person*> person_vec = person_service->get_person_vec();
+        for (int i = 0; i < person_vec.size(); i++)
+        {
+            if (person_vec[i]->get_name() == name)
+            {
+                int id = person_vec[i]->get_id();
+                stringstream ss;
+                ss << id;
+                string str_id = ss.str();
+                person_service->remove_person(str_id);
+            }
+        }
 
         display_person_list();
     }
@@ -196,7 +210,20 @@ void MainWindow::on_remove_pushButton_clicked()
         ui->see_more_view_person->hide();
         ui->description_output_computer->clear();
         currently_chosen_entry = ui->table_view_computers->currentIndex().row();
-        Computer* current_computer = computer_vector.at(currently_chosen_entry);
+        QTableWidgetItem *entry_name = ui->table_view_computers->item(currently_chosen_entry, 0);
+        string name = entry_name->text().toStdString();
+        vector<Computer*> computer_vec = computer_service->get_computer_vec();
+        for (int i = 0; i < computer_vec.size(); i++)
+        {
+            if (computer_vec[i]->get_name() == name)
+            {
+                int id = computer_vec[i]->get_id();
+                stringstream ss;
+                ss << id;
+                string str_id = ss.str();
+                computer_service->remove_computer(str_id);
+            }
+        }
 
         display_computer_list();
     }
@@ -224,6 +251,5 @@ void MainWindow::on_add_pushButton_clicked()
 
         int addConnectionsReturnValue = newadd_connections.exec();
     }
-
 }
 

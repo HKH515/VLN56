f3db6c2335af62e7132d10369e6a0e620474472e
Hannes Kristján Hannesson <hanneskh15@ru.is>
2015-12-14 16:01:09 +0000

    added functionality to add button in add connections

--- add_connections.h
+++ add_connections.h
@@ -2,6 +2,11 @@
 #define ADD_CONNECTIONS_H
 
 #include <QDialog>
+#include <string>
+#include <vector>
+#include "connectionsservice.h"
+
+using namespace std;
 
 namespace Ui {
 class add_connections;
@@ -15,8 +20,13 @@ public:
     explicit add_connections(QWidget *parent = 0);
     ~add_connections();
 
+private slots:
+    void on_pushbutton_cancel_clicked();
+    void on_pushbutton_add_clicked();
+
 private:
     Ui::add_connections *ui;
+    ConnectionsService* connections_service;
 };
 
 #endif // ADD_CONNECTIONS_H
